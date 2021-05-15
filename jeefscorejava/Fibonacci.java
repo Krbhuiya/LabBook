68764c24f408c681efd5d9cfde7755794405ad91
Bikramjit <bikramjitbhuiya97@gmail.com>
2021-05-15 21:35:24 +0530

    update file

--- jeefscorejava/Fibonacci.java
+++ jeefscorejava/Fibonacci.java
@@ -7,35 +7,34 @@ package jeefscorejava;
 import java.util.Scanner;
 public class Fibonacci {
 	static int n1=0,n2=1,n3; 
-	public static void Recursive(int n)
+	public static int Recursive(int n)
 	{
 		if(n > 0)
 		{
 			n3 = n1 + n2;
 			n1 = n2;
 			n2 = n3;
-			System.out.print(" "+n3);
 			Recursive(n-1);
-		}	
+		}
+		return n3;
 	}
 	public static void NonRecursive(int n)
 	{
-		int f1=0,f2=1,f3;
+		int f1=0,f2=1,f3=0;
 		System.out.println("Non-Recursive:");
-		System.out.print(f2+" ");
 		for(int i=0; i<n-1; i++)
 		{
 			f3 = f1 + f2;
-			System.out.print(f3+" ");
 			f1 = f2;
 			f2 = f3;
 		}
-		
+		System.out.print(f3+" ");
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your choice: ");
 		int choice = sc.nextInt();
 		switch(choice)
 		{
@@ -47,8 +46,8 @@ public class Fibonacci {
 		case 2:
 			System.out.println("Enter a number: ");
 			int n1 = sc.nextInt();
-			System.out.print(1);
-			Recursive(n1-1);
+			int n3 = Recursive(n1-1);
+			System.out.print(n3);
 			break;
 		default :
 			System.out.println("Wrong Choice.");
