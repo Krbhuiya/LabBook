baa72ad43e6cd49a43a7f717c7cfdb32124061b8
Krbhuiya <40709868+Krbhuiya@users.noreply.github.com>
2021-05-22 15:44:43 +0530

    Add files via upload

---
+++ com/capgemini/lab5/q1/AgeValidationException.java
@@ -0,0 +1,10 @@
+package com.capgemini.lab5.q1;
+
+public class AgeValidationException extends Exception{
+
+	public AgeValidationException()
+	{
+		System.out.println("Exceotion: Age of a person should be above 15");
+	}
+}
+
---
+++ com/capgemini/lab5/q3/EmployeeException.java
@@ -0,0 +1,9 @@
+package com.capgemini.lab5.q3;
+
+public class EmployeeException extends Exception{
+
+	public EmployeeException()
+	{
+		System.out.println("Exception: Salary need to be more than 3000");
+	}
+}
